8bb84d8c9c345e32a7cb32aa393a843ce61832af
Tam Nguyen <ntamvl@gmail.com>
2021-12-21 19:14:56 +0700

    Update readme

--- app/javascript/react/components/App/index.js
+++ app/javascript/react/components/App/index.js
@@ -7,10 +7,12 @@ export const App = () => {
   return (
     <div className="container tw-bg-gray-700 tw-rounded-xl tw-py-4">
       <div className={'tw-py-4'}>
-        <div className={'my_styles_3'}>Hello React!!! 000</div>
-        <div className={'my_styles_4'}>Styles SCSS</div>
+        <div className={'my_styles_3'}>Hello React!!! ^_^</div>
       </div>
       <MyClock />
+      <div className={'tw-py-4'}>
+        <div className={'my_styles_4'}>Styles SCSS</div>
+      </div>
     </div>
   )
 }
